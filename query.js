3497190c1659673372092f6c9d817540f73914fa
Mayank Srivastav <mayanksri18@yahoo.in>
2017-08-07 20:42:03 +0530

    Added module & global vars

--- query.js
+++ query.js
@@ -6,3 +6,16 @@
 
   Date: 27-June-2017
 */
+
+(function (windo) {
+  "use strict";
+  // Global shorthands
+  let _arr = [],
+    _keys = Object.keys,
+    _values = Object.values,
+    _splice = _arr.splice,
+    _proto = Object.prototype,
+    _filter = _arr.filter,
+    _map = _arr.map,
+    _document = window.document;
+})(window);
